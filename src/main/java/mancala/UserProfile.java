fe7ffc499030393039329cc54caa70dc429b204a
Pranay Harpalani Dubey <pharpala@uoguelph.ca>
2023-12-02 02:53:51 -0500

    Update UserProfile.java

--- src/main/java/mancala/UserProfile.java
+++ src/main/java/mancala/UserProfile.java
@@ -4,9 +4,47 @@ import java.io.Serializable;
 
 public class UserProfile implements Serializable {
 
+    private static final long serialVersionUID = 10;
     private String userName;
     private int kalahGamesPlayed;
     private int ayoGamesPlayed;
     private int kalahGamesWon;
     private int ayoGamesWon;
+
+    private int gamesWon;
+    private int gamesPlayed;
+
+    //getters
+    public String getName() {
+        return userName;
+    }
+
+    public int getGamesWon() {
+        return gamesWon;
+    }
+
+    public int getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    //incrementers
+    public void addName(String user) {
+        this.userName = user;
+    }
+
+    public void incrementKalahGamesPlayed() {
+        kalahGamesPlayed++;
+    }
+
+    public void incrementAyoGamesPlayed() {
+        ayoGamesPlayed++;
+    }
+
+    public void incrementGamesWon() {
+        this.gamesWon++;
+    }
+
+    public void incrementGamesPlayed() {
+        this.gamesPlayed++;
+    }
 }
